00241bc1d9b3a46fb5612bb145de35b70ac2042b
Gabriel Nascimento <gabrieln99626@gmail.com>
2026-01-27 15:18:24 -0300

    feat(battle/characters): integrar uso de magia em Batalha e adicionar Guerreiro
    - Batalha.atacar1v1 tenta usarMagia() e aplica dano mágico se houver mana; caso contrário usa atacar() (físico).
    - Adicionada subclasse Guerreiro com status padrão como exemplo.
    - Compatibilizado com Personagem.usarMagia() (retorna List<Integer>: dano, sucesso).

--- src/battle/batalha.java
+++ src/battle/batalha.java
@@ -3,9 +3,18 @@ package battle;
 import characters.Personagem;
 
 public class Batalha {
-    public static double atacar1v1(Personagem atacante, Personagem alvo) {
-        double dano = atacante.atacar();
-        alvo.receberDano(dano);
-        return dano;
+
+    public static int atacar1v1(Personagem atacante, Personagem alvo) {
+        List<Integer> resultadoMagia = atacante.usarMagia();
+        //Se personagem tem mana ele usará magia
+        if (resultadoMagia.get(1) == 1) {
+            int danoMagico = resultadoMagia.get(0);
+            alvo.receberDano(danoMagico, true);
+            return danoMagico;
+        } else {
+            int dano = atacante.atacar();
+            alvo.receberDano(dano);
+            return dano;
+        }
     }
 }
---
+++ src/characters/Guerreiro.java
@@ -0,0 +1,9 @@
+package characters;
+
+public class Guerreiro extends Personagem {
+    public Guerreiro(String nome) {
+        //nome, vida, mana, dano, manaRegen, defesa, critico
+        //TODO: Adicionar ramdom nos status
+        super(nome, 20, 0, 4, 0, 3, 0);
+    }
+}
