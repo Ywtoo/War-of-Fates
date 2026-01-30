cb8258ff114fcbdd28a5a076d95e93526ff24ef0
Aurora <168770777+Margheo@users.noreply.github.com>
2026-01-29 22:52:54 -0300

    rebalanceamento

--- src/characters/Guerreiro.java
+++ src/characters/Guerreiro.java
@@ -8,12 +8,12 @@ public class Guerreiro extends Personagem {
         // (String nome, int vida,  int dano, int defesa, int critico, int mana, int custoMana, int manaRegen)
         super(
                 nome,
-                20 + statusAleatorio(3),
-                4 + statusAleatorio(1),  
-                3 + statusAleatorio(1), 
+                16 + statusAleatorio(5),
+                5 + statusAleatorio(2),  
+                5 + statusAleatorio(4), 
                 0,
                 0,
-                10,
+                0,
                 0
         );
     }
